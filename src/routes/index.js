178ae5c0abaaba6cded0936007a373cb6e3f9a18
Ralac <andhyralac11@gmail.com>
2020-10-14 08:35:34 -0600

    Se agrego la carpeta routes el archivo index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index.html', { title: 'Pagina Web con Node' });
+});
+
+
+router.get('/contact', (req, res) => {
+    res.render('contact.html', { title: 'Pagina Contacto' });
+});
+
+
+
+module.exports = router;
